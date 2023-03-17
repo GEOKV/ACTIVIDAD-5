1f3d79eee6f2ec06a316ba7431fce0293248ba43
GEOKV <ge-obany@hotmail.com>
2023-03-16 23:50:24 -0600

    Actividad_5

---
+++ ARRAY_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+main() {
+	int tam=0;
+	cout<<"Cuantas notas desea ingresar: ";
+	cin>>tam;
+	int notas[tam];
+	for (int i=0;i<tam;i++){
+		cout<<"Ingrese Nota "<<i+1<<":";
+		cin>>notas[i];
+	}
+		
+	for (int i=0;i<tam;i++){
+		cout<<notas[i]<<endl;	
+	}	
+		
+	system("pause");
+}
